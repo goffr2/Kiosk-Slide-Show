9ccf698d169234b40129486bb56c2b5f92398e5b
Ricky Goff <55897197+goffr2@users.noreply.github.com>
2019-10-28 11:14:53 -0400

    initial Commit

---
+++ powerpoint.ps1
@@ -0,0 +1,23 @@
+﻿New-Item -Force C:\Users\kuser\Slideshow -Type Directory
+New-Item -Force C:\Users\kuser\Slideshow\logs -Type Directory
+
+copy-item \\itnas\netshare\KioskSlideShow\Slideshow.ppsx C:\Users\kuser\Slideshow\Slideshow.ppsx -Recurse -Force
+Start-Process C:\Users\kuser\Slideshow\Slideshow.ppsx
+$A = 1
+While ($A = 2)
+{
+$LastWritten = Get-Item \\itnas\netshare\KioskSlideShow\Slideshow.ppsx |Select-Object -ExpandProperty  LastWriteTime
+start-sleep -Seconds 10
+$CurrentWritten = Get-Item \\itnas\netshare\KioskSlideShow\Slideshow.ppsx |Select-Object -ExpandProperty  LastWriteTime
+if ($CurrentWritten -ne $LastWritten)
+{
+	$Process = Get-Process POWERPNT |select-object -ExpandProperty Id
+	Stop-Process $Process
+	copy-item \\itnas\netshare\KioskSlideShow\Slideshow.ppsx C:\Users\kuser\Slideshow\Slideshow.ppsx -Recurse -Force
+	Start-Process C:\Users\kuser\Slideshow\Slideshow.ppsx
+}
+else{
+
+}
+
+}
